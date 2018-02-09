f6587fab5d7d354f3831e495bc5b5560e23756c3
Vargese Antony <Vargese.antony@hotmail.com>
2018-02-09 16:12:13 -0600

    Watching and responding to routes using events

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,5 +1,12 @@
-import { Component } from '@angular/core';
-import { Router } from '@angular/router';
+import { Component, OnInit } from '@angular/core';
+import { 
+    Router, 
+    Event, 
+    NavigationStart, 
+    NavigationEnd, 
+    NavigationCancel, 
+    NavigationError 
+} from '@angular/router';
 
 import { AuthService } from './user/auth.service';
 
@@ -7,13 +14,31 @@ import { AuthService } from './user/auth.service';
     selector: 'pm-app',
     templateUrl: './app.component.html'
 })
-export class AppComponent {
+export class AppComponent implements OnInit{
     pageTitle: string = 'Acme Product Management';
+    loading: boolean = false;
 
     constructor(
         private authService: AuthService,
         private router: Router
     ) { }
+
+    ngOnInit() : void {
+        this.router.events.subscribe( (routerEvent: Event) => {
+            this.setLoadingProperty(routerEvent);
+        });
+    }
+    
+    setLoadingProperty(routerEvent:Event) {
+        if(routerEvent instanceof NavigationStart) {
+            this.loading = true;
+        } else if( routerEvent instanceof NavigationCancel || 
+            routerEvent instanceof NavigationError || 
+            routerEvent instanceof NavigationEnd) 
+        {
+            this.loading = false;
+        }
+    }
 
     logOut(): void {
         this.authService.logout();
